93f1991ea77437209a4921fc09aa743f14cfa2e9
fxliang <fx.liang@outlook.com>
2024-07-11 17:06:38 +0800

    build: update linux build script build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-g++ -Wall -llua5.4 -lrime main.cpp -o rimeac.lua -std=c++17
+if command -v ninja >/dev/null 2>&1; then
+  echo "ninja is available."
+  cmake -GNinja -Bbuild . -DCMAKE_BUILD_TYPE=Release
+else
+  echo "ninja is not available."
+  cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release
+fi
+cmake --build build
+cp build/rimeac.lua ./
+cp build/librimeac.so ./
