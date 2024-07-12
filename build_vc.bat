e4c7bb1fab2d5df7c4f7a051f42e15ba8c72560d
fxliang <fx.liang@outlook.com>
2024-07-12 17:21:14 +0800

    ci: turn back to cmake and ninja

--- build_vc.bat
+++ build_vc.bat
@@ -1,33 +1,9 @@
 @echo off
 set base_dir=%CD%
 
-rem build x64 and x86 version of rimeac.lua
-xmake --version >nul 2>&1
-if not errorlevel 1 (
-  goto xmake
-)
 call :build x64
 call :build x86
 goto end
-
-:xmake
-  if not defined VSCMD_ARG_TGT_ARCH (
-    call msvc-latest.bat x64
-  )
-  echo VCINSTALLDIR defined
-  xmake f -a x64
-  if not errorlevel 0 goto error
-  xmake
-  if not errorlevel 0 goto error
-  xmake install
-  if not errorlevel 0 goto error
-  xmake f -a x86
-  if not errorlevel 0 goto error
-  xmake
-  if not errorlevel 0 goto error
-  xmake install
-  if not errorlevel 0 goto error
-  goto end
 
 rem param %1 should be 32 / 64, to make x86 and x64
 :build
